4b959805a13e034bfe44ca7b7eaecc57a945176e
Rune Hou Thode <runehouthode@gmail.com>
2018-11-21 21:59:36 +0100

    Prøver at få migrering til at virke

--- WebApplication1/Migrations/Configuration.cs
+++ WebApplication1/Migrations/Configuration.cs
@@ -22,24 +22,24 @@ namespace GameWebApplication.Migrations
                 context.Users.Add(usr1); context.Users.Add(usr1);
             }
 
-            Character character1 = new Character { Name = "MonsterSlayer", UserId = 1, User = usr1,  };
-            Character character2 = new Character { Name = "Kirneh", UserId = 2, User = usr2 };
-
-            if (!context.Characters.Any())
-            {
-                context.Characters.Add(character1); context.Characters.Add(character2);
-            }
-
             Inventory inventory1 = new Inventory { Gold = 30 };
-            Inventory inventory2 = new Inventory { Gold = 197 };
+            Inventory inventory2 = new Inventory { Gold = 231 };
 
             if (context.Inventory.Any())
             {
                 context.Inventory.Add(inventory1); context.Inventory.Add(inventory2);
             }
 
+            Character character1 = new Character { Name = "MonsterSlayer", UserId = usr2.Id, InventoryId = inventory1.Id };
+            Character character2 = new Character { Name = "Kirneh", UserId = usr1.Id, InventoryId = inventory2.Id };
+
+            if (!context.Characters.Any())
+            {
+                context.Characters.Add(character1); context.Characters.Add(character2);
+            }
+
             Equipment item1 = new Equipment { Name = "Reaper of Souls", InventoryId = inventory1.Id };
-            Equipment item2 = new Equipment { Name = "Psykosværd", InventoryId = inventory1.Id };
+            Equipment item2 = new Equipment { Name = "Psykosvaerd", InventoryId = inventory1.Id };
 
             if (context.Equipment.Any())
             {
